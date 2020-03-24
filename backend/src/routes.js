e362c2e67514eacd6507a5d64e8aa77066c36656
Rui Richard Blaese <ruiblaese@gmail.com>
2020-03-24 13:06:42 -0300

    first day, backend

---
+++ backend/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+//Session
+routes.post('/sessions', SessionController.create);
+
+//ONG
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+//Incidents
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+//Profile
+routes.get('/profile', ProfileController.index);
+
+
+module.exports = routes;
